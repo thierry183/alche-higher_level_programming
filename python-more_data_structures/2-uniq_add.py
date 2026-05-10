38eac014be58a7e5fa5a8c1614261ef3bdf037b9
root <t.muhire1@alustudent.com>
2026-05-10 04:02:17 -0700

    q

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    unique = []
+    for i in range(len(my_list)):
+        if my_list[i] not in unique:
+            sum += my_list[i]
+            unique.append(my_list[i])
+    return sum
+uniq_add([1,2,3,4,5,1])
