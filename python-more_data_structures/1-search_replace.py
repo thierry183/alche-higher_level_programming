7f98831b948e5466e4fb373f2ed0feafd22d88d2
root <t.muhire1@alustudent.com>
2026-05-10 04:17:49 -0700

    w

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list ,search ,replace):
+def search_replace(my_list,search,replace):
     new_list = my_list[:]
     for i in range(len(my_list)):
         if my_list[i] == search:
