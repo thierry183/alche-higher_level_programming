d9442aa2f1aa02202ddf36a5a99d41abee8320a7
root <t.muhire1@alustudent.com>
2026-05-10 04:00:45 -0700

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    return (len(sentence), sentence[0])
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list[i] = replace
+    return new_list
